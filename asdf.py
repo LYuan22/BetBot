deb742bbfe8505393ad81f536f935a049da287f3
LYuan22 <lucasyuan1528@gmail.com>
2021-06-28 18:13:30 -0400

    deleting asdf

--- asdf.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api-nba-v1.p.rapidapi.com/games/seasonYear/%7Bseasonyear%7D"
-
-headers = {'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'}
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
